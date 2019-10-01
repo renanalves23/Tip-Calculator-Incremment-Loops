e42bd6e54b49357e14d7be79c6e8c08e60ee231f
renan <renanalves23@outlook.com>
2019-10-01 15:51:10 -0300

    chore: Adição de cálculos das gorjetas de Mike

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// var pencent;
-// var tip;
 var john = {
   bills: [124, 48, 268, 180, 42],
   fullname: 'John Smith',
@@ -24,25 +22,34 @@ var john = {
       this.finalValues[i] = this.bills[i] + this.bills[i] * percentage;
     }
   }
-  
 }
+
+var mike = {
+  bills: [77, 475, 110, 45],
+  fullname: 'Mike Jenkins',
+  calcTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      //Determinar percentual
+      var percentage;
+      //var bill = this.bills[i]; //caso quiser usar direto bill nas condições
+
+      if (this.bills[i] < 100) {
+        percentage = .2;
+      }else if (this.bills[i] >= 100 && this.bills[i] < 300) {
+        percentage = .1;
+      }else {
+        percentage = .25;
+      }
+      //resultados nas arrays vazias
+      this.tips[i] = this.bills[i] * percentage;
+      this.finalValues[i] = this.bills[i] + this.bills[i] * percentage;
+    }
+  }
+}
+
 john.calcTips();
-console.log(john);
-  // for (var i = 0; i < bills.length; i++) {
-  //   console.log(bills[i]);
-  //   if(bills[i] < 50) {
-  //     percent = 0.2
-  //     tip = bills[i] * percent
-  //     console.log('O valor da gorjeta para a conta de ' + bills[i] + ' é  de ' + tip);      
-  //   }else if (bills[i] >= 50 && bills[i] < 200) {
-  //     percent = 0.15
-  //     tip = bills[i] * percent
-  //     console.log('O valor da gorjeta para a conta de ' + bills[i] + ' é  de ' + tip);
-  //   }else {
-  //     percent = 0.1
-  //     tip = bills[i] * percent
-  //     console.log('O valor da gorjeta para a conta de ' + bills[i] + ' é  de ' + tip);
-  //   };
-  // }
-  //console.log('Hello');
-  //console.log(bills[i]);   
+mike.calcTips();
+console.log(john, mike);
